735d48abced04450bdb9c157d3a052d8e0c9f2b6
Cl4isen <165159204+Aventern@users.noreply.github.com>
2024-06-12 10:54:02 +0900

    Update and rename index.py to example.html

--- index.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-with open("IMG_5029.jpeg", "rb") as image_file:
-    base64_string = base64.b64encode(image_file.read()).decode('utf-8')
-
-# Base64エンコードされた文字列を表示
-print(base64_string)
